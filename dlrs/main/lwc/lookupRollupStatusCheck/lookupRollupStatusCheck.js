7e835e443d70b07bd680bf6bc52806997a818869
Scott Fenton <sfenton3@users.noreply.github.com>
2023-03-29 15:56:19 -0400

    Add methods and mock data

--- dlrs/main/lwc/lookupRollupStatusCheck/lookupRollupStatusCheck.js
+++ dlrs/main/lwc/lookupRollupStatusCheck/lookupRollupStatusCheck.js
@@ -1,3 +1,39 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import getAllScheduledItems from '@salesforce/apex/LookupRollupStatusCheckController.getAllScheduledItems';
+import getSpecificScheduledItems from '@salesforce/apex/LookupRollupStatusCheckController.getSpecificScheduledItems';
+import hasChildTriggers from '@salesforce/apex/LookupRollupStatusCheckController.hasChildTriggers';
 
-export default class LookupRollupStatusCheck extends LightningElement {}
+
+
+
+export default class LookupRollupStatusCheck extends LightningElement {
+
+    @track recordCount;
+    @track recordCountAll;
+    @track error;
+
+    handleLoad(){
+        getAllScheduledItems()
+            .then(result => {
+                this.recordCount = result;
+                console.log(result);
+            })
+            .catch(error => {
+                this.error = error;
+                console.log(error);
+        });
+
+        getSpecificScheduledItems()
+        .then(result => {
+            this.recordCountAll = result;
+            console.log(result);
+        })
+        .catch(error => {
+            this.error = error;
+            console.log(error);
+    });
+    hasChildTriggers()
+
+    }
+
+}
