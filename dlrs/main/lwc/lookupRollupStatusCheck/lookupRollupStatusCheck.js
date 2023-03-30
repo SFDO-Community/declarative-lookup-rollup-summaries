2ffc0f587544d4cd99d011718d4ca08b4b69b809
Scott Fenton <sfenton3@users.noreply.github.com>
2023-03-30 03:08:08 -0400

    status update

--- dlrs/main/lwc/lookupRollupStatusCheck/lookupRollupStatusCheck.js
+++ dlrs/main/lwc/lookupRollupStatusCheck/lookupRollupStatusCheck.js
@@ -3,13 +3,11 @@ import getAllScheduledItems from '@salesforce/apex/LookupRollupStatusCheckContro
 import getSpecificScheduledItems from '@salesforce/apex/LookupRollupStatusCheckController.getSpecificScheduledItems';
 import hasChildTriggers from '@salesforce/apex/LookupRollupStatusCheckController.hasChildTriggers';
 
-
-
-
 export default class LookupRollupStatusCheck extends LightningElement {
 
-    @track recordCount;
-    @track recordCountAll;
+    @track recordCount = '0';
+    @track recordCountAll = '0';
+    @track triggerCount = '0';
     @track error;
 
     handleLoad(){
@@ -31,9 +29,17 @@ export default class LookupRollupStatusCheck extends LightningElement {
         .catch(error => {
             this.error = error;
             console.log(error);
-    });
-    hasChildTriggers()
+        });
 
+        hasChildTriggers()
+        .then(result => {
+            this.triggerCount = result;
+            console.log(result);
+        })
+        .catch(error => {
+            this.error = error;
+            console.log(error);
+        });
     }
 
 }
