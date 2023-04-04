a0834065ae978d72816721472d4bbaef57abcd6d
Khang Ly <lynguyenkhang285@gmail.com>
2023-04-04 23:26:59 +0700

    update RollupStatusCheck component UI

--- dlrs/main/lwc/rollupStatusCheck/rollupStatusCheck.js
+++ dlrs/main/lwc/rollupStatusCheck/rollupStatusCheck.js
@@ -1,4 +1,5 @@
-import { LightningElement, api } from "lwc";
+import { LightningElement, api, wire, track } from "lwc";
+
 import getAllScheduledItems from "@salesforce/apex/LookupRollupStatusCheckController.getAllScheduledItems";
 import getSpecificScheduledItems from "@salesforce/apex/LookupRollupStatusCheckController.getSpecificScheduledItems";
 import hasChildTriggerDeployed from "@salesforce/apex/LookupRollupStatusCheckController.hasChildTriggerDeployed";
@@ -7,9 +8,13 @@ import getScheduledFullCalculates from "@salesforce/apex/LookupRollupStatusCheck
 import getCalculateJobs from "@salesforce/apex/LookupRollupStatusCheckController.getCalculateJobs";
 import getScheduledJobs from "@salesforce/apex/LookupRollupStatusCheckController.getScheduledJobs";
 
+const mockLookupRollupSummary2 = '';
+
 export default class rollupStatusCheck extends LightningElement {
-  // Get record lookup from parent component
-  _name;
+
+  // Khang Ly contribute
+  header = 'Status Check';  
+
   lookupID;
 
   // General status check variables
@@ -23,134 +28,71 @@ export default class rollupStatusCheck extends LightningElement {
   parentTrigger = "NA";
   childTrigger = "NA";
   calculateJobError = "No Errors Found";
-
+  
   // Error Handling
   error = [];
 
   connectedCallback() {
     console.log("connectedCallback");
     console.log("my lookup id is :" + this._name);
+    this.allScheduleItems();
+    this.scheduleJobs();
+  } 
 
-    if (this._name == null) {
-      // General Status Checks
-      this.allScheduleItems();
-      this.scheduleJobs();
-    } else {
-      // General Status Checks
-      this.allScheduleItems();
-      this.scheduleJobs();
 
-      // Rollup Specific Status Checks
-      this.specificScheduleItems();
-      this.childTriggers();
-      this.parentTriggers();
-      this.scheduledFullCalculate();
-      this.calculateJobs();
-    }
-  }
-
+  // Get record lookup from parent component
+  @track _name = mockLookupRollupSummary2;
   @api
-  get name() {
-    return this._name;
+  get name() { 
+    return this._name; 
   }
-
-  set name(value) {
-    // Rollup Specific Status Checks
-    this._name = value;
-    this.specificScheduleItems();
-    this.childTriggers();
-    this.parentTriggers();
-    this.scheduledFullCalculate();
-    this.calculateJobs();
+  set name(value) { 
+    this._name = value; 
   }
 
   // Method to check if there are any failed calculate jobs for the specific rollup
-  calculateJobs() {
-    console.log("calculateJobs");
-    getCalculateJobs({ lookupID: this._name })
-      .then((result) => {
-        this.calculateJobError = result;
-      })
-      .catch((error) => {
-        this.error.push(error);
-        console.log(error);
-      });
-  }
+  @wire(getCalculateJobs, { lookupID : '$_name'})
+  wiredGetCalculateJobs({error, data}){ this.handleApexResponse(data, error, 'calculateJobError'); }
 
   // Method to check if there are any scheduled full calculates for the specific rollup
-  scheduledFullCalculate() {
-    console.log("scheduledFullCalculate");
-    getScheduledFullCalculates({ lookupID: this._name })
-      .then((result) => {
-        this.nextFullCalculateDate = result;
-      })
-      .catch((error) => {
-        this.error.push(error);
-        console.log(error);
-      });
-  }
+  @wire(getScheduledFullCalculates, { lookupID : '$_name'})
+  wiredGetScheduledFullCalculates({error, data}){ this.handleApexResponse(data, error, 'nextFullCalculateDate'); }
 
   // Method to check if there are any scheduled items for the specific rollup
-  specificScheduleItems() {
-    console.log("specificScheduleItems");
-    getSpecificScheduledItems({ lookupID: this._name })
-      .then((result) => {
-        this.recordCount = result;
-      })
-      .catch((error) => {
-        this.error.push(error);
-        console.log(error);
-      });
-  }
+  @wire(getSpecificScheduledItems, { lookupID : '$_name'})
+  wiredGetSpecificScheduledItems({error, data}){ this.handleApexResponse(data, error, 'recordCount'); }
 
   // Method to check if child triggers are present
-  childTriggers() {
-    console.log("childTriggers");
-    hasChildTriggerDeployed({ lookupID: this._name })
-      .then((result) => {
-        this.childTrigger = result;
-      })
-      .catch((error) => {
-        this.error.push(error);
-        console.log(error);
-      });
-  }
+  @wire(hasChildTriggerDeployed, { lookupID : '$_name'})
+  wiredHasChildTriggerDeployed({error, data}){ this.handleApexResponse(data, error, 'childTrigger'); }
 
   // Method to check if child triggers are present
-  parentTriggers() {
-    console.log("childTriggers");
-    hasParentTriggerDeployed({ lookupID: this._name })
-      .then((result) => {
-        this.parentTrigger = result;
-      })
-      .catch((error) => {
-        this.error.push(error);
-        console.log(error);
-      });
-  }
+  @wire(hasParentTriggerDeployed, { lookupID : '$_name' })
+  wiredHasParentTriggerDeployed({ error, data }){ this.handleApexResponse(data, error, 'parentTrigger'); }
 
   // General status method to grab all scheduled items regardless of rollup
-  allScheduleItems() {
-    console.log("allScheduleItems");
-    getAllScheduledItems()
-      .then((result) => {
-        this.recordCountAll = result;
-      })
-      .catch((error) => {
-        this.error.push(error);
-        console.log(error);
-      });
+  async allScheduleItems() {
+    let data, error;
+    try{ data = await getAllScheduledItems(); } 
+    catch(err){ error = err }
+    this.handleApexResponse(data, error, 'recordCountAll');
   }
 
-  scheduleJobs() {
-    console.log("All Scheduled Cron Jobs");
-    getScheduledJobs()
-      .then((result) => {
-        this.scheduledCronJobs = result;
-      })
-      .catch((error) => {
-        this.error.push(error);
-        console.log(error);
-      });
+  async scheduleJobs() {
+    let data, error;
+    try{ data = await getScheduledJobs(); } 
+    catch(err){ error = err }
+    this.handleApexResponse(data, error, 'scheduledCronJobs');
+  }
+
+// *** Utility ***
+  handleApexResponse(data, error, propertyName){
+    if(data){
+      console.log(propertyName + '- data: ' + data);
+      this[propertyName] = data;
+    } else if(error){
+      console.log(propertyName + '- error: ' + error);
+      this.error.push(error);
+    }
   }
 }
