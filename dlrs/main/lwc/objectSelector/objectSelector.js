0bd94a57e9ec908598f47fc5c7c8bc6390197cb6
Anthony Heber <anthony.heber@gmail.com>
2023-04-19 17:44:31 -0600

    Improve Object Picker behaviors and add features
    Improve tab behavior and restructure data so it is easier to work with.
    Enable correct icon per object (delayed loading)
    Enable correct loading and clearing of values from parent
    Improve search perf and sorting

--- dlrs/main/lwc/objectSelector/objectSelector.js
+++ dlrs/main/lwc/objectSelector/objectSelector.js
@@ -1,53 +1,153 @@
-import { LightningElement, api } from "lwc";
-
+import { LightningElement, api, wire } from "lwc";
 import getAllObjects from "@salesforce/apex/ObjectSelectorController.getParentObjList";
+import jsLevenshtein from "./js-levenshtein";
+import { getObjectInfos } from "lightning/uiObjectInfoApi";
 
-const DELAY = 300; // delay apex callout timing in miliseconds
+const DELAY = 100; // delay apex callout timing in miliseconds
+const MAX_RESULTS = 5;
 export default class ObjectSelector extends LightningElement {
   // public properties with initial default values
   @api label = "Object";
-  @api placeholder = "Search...";
-  @api iconName = "standard:account";
-  @api currentSelection;
+  @api placeholder = "Select on object";
+  @api iconName = "standard:custom"; // would be awesome to use the correct icon
+  _currentSelection;
+  @api
+  get currentSelection() {
+    return this._currentSelection;
+  }
+  set currentSelection(val) {
+    this._currentSelection = val;
+    this.selectObject(val);
+  }
   // private properties
+  objectApiNames = [];
+  objectIconCache;
   lstResult = []; // to store list of returned records
-  hasRecords = true;
   searchKey = ""; // to store input field value
   isSearchLoading = false; // to control loading spinner
   delayTimeout;
-  selectedRecord = {}; // to store selected lookup record in object formate
-  mapData = [];
+  selectedRecord = "";
+  selectedRecordIconName = this.iconName;
+  objects;
+  preventBlur = false;
   connectedCallback() {
+    this.isSearchLoading = true;
     //GET LIST OF ALL OBJECTS
     getAllObjects()
-      .then((result) => {
-        if (result != null) {
-          this.mapData = JSON.parse(JSON.stringify(result));
+      .then((objects) => {
+        if (objects != null) {
+          this.objects = objects.map((o) => {
+            return { ...o, iconName: this.iconName };
+          });
+          this.objectApiNames = objects.map((o) => o.fullName);
+          if (this.currentSelection) {
+            this.selectObject(this.currentSelection);
+          }
         }
       })
       .catch((error) => {
         this.error = error;
-        this.mapData = {};
+        this.objects = {};
+      })
+      .finally(() => {
+        this.isSearchLoading = false;
       });
+  }
+
+  // use the UI API to determine the iconName for each object
+  // this call is slow and if you're interacting with the UI before this loads
+  // you'll get the generic/fallback icon
+  @wire(getObjectInfos, { objectApiNames: "$objectApiNames" })
+  wiredObjectInfos({ data }) {
+    if (data) {
+      // try and extract the Icon path from
+      const iconNameRegEx = /(\w+)\/(\w+)(?:_\d+)\./;
+      const objectIconCache = data.results
+        .filter((r) => r.statusCode === 200)
+        .filter((r) => r.result.themeInfo)
+        .reduce((t, v) => {
+          t[v.result.apiName] = v.result?.themeInfo;
+          return t;
+        }, {});
+      this.objects.forEach((o) => {
+        if (!objectIconCache[o.fullName]?.iconUrl) {
+          return;
+        }
+        o.iconUrl = objectIconCache[o.fullName].iconUrl;
+        o.iconColor = `#${objectIconCache[o.fullName].color}`;
+        o.iconStyle = `background-color: #${
+          objectIconCache[o.fullName].color
+        };`;
+        if (o.iconUrl) {
+          const matches = iconNameRegEx.exec(o.iconUrl);
+          if (matches) {
+            o.iconName = `${matches[1]}:${matches[2]}`;
+          }
+        }
+      });
+    }
+  }
+
+  get isReady() {
+    return !this.objects;
   }
 
   // update searchKey property on input field change
   handleKeyChange(event) {
-    // Debouncing this method: Do not update the reactive property as long as this function is
-    // being called within a delay of DELAY. This is to avoid a very large number of Apex method calls.
-
-    var matchedRecords = [];
-    this.isSearchLoading = true;
     window.clearTimeout(this.delayTimeout);
     const searchKey = event.target.value;
+    // eslint-disable-next-line @lwc/lwc/no-async-operation
     this.delayTimeout = setTimeout(() => {
-      matchedRecords = this.mapData.filter(function (link) {
-        return link.toLowerCase().includes(searchKey.toLowerCase());
+      // Require at least two character for matches
+      if (searchKey.trim().length < 2) {
+        this.lstResult = [];
+        return;
+      }
+      // constrain results to those containing the text
+      this.lstResult = this.objects.filter(function (obj) {
+        return (
+          obj.fullName.toLowerCase().includes(searchKey.toLowerCase()) ||
+          obj.label.toLowerCase().includes(searchKey.toLowerCase())
+        );
       });
-
-      this.hasRecords = matchedRecords.length == 0 ? false : true;
-      this.lstResult = JSON.parse(JSON.stringify(matchedRecords));
+      // sort by most similar, either label or API name
+      this.lstResult.sort((a, b) => {
+        return (
+          // take the nearest of the Label and API name
+          Math.min(
+            jsLevenshtein(searchKey.toLowerCase(), `${a.fullName}`),
+            jsLevenshtein(searchKey.toLowerCase(), `${a.label}`)
+          ) -
+          Math.min(
+            jsLevenshtein(searchKey.toLowerCase(), `${b.fullName}`),
+            jsLevenshtein(searchKey.toLowerCase(), `${b.label}`)
+          )
+        );
+      });
+      // limit to the MAX_RESULTS closest matches
+      this.lstResult.splice(MAX_RESULTS);
     }, DELAY);
+  }
+
+  showResultList() {
+    const lookupInputContainer = this.template.querySelector(
+      ".lookupInputContainer"
+    );
+    const clsList = lookupInputContainer.classList;
+    clsList.add("slds-is-open");
+  }
+
+  hideResultList() {
+    // Allow enough time for the click handler of the object list to run before we burn the elements
+    // 310 is chosen because on some mobile devices clicks can take up to 300ms to fire
+    // eslint-disable-next-line @lwc/lwc/no-async-operation
+    setTimeout(() => {
+      const lookupInputContainer = this.template.querySelector(
+        ".lookupInputContainer"
+      );
+      const clsList = lookupInputContainer.classList;
+      clsList.remove("slds-is-open");
+    }, 310);
   }
 
   // method to toggle lookup result section on UI
@@ -64,30 +164,54 @@ export default class ObjectSelector extends LightningElement {
       case "lookupContainer":
         clsList.remove("slds-is-open");
         break;
+      default:
+        break;
     }
   }
 
   // method to clear selected lookup record
   handleRemove() {
     this.searchKey = "";
-    this.selectedRecord = {};
+    this.selectedRecord = "";
+    this.lstResult = [];
     this.lookupUpdatehandler(undefined); // update value on parent component as well from helper function
 
     // remove selected pill and display input field again
     const searchBoxWrapper = this.template.querySelector(".searchBoxWrapper");
-    searchBoxWrapper.classList.remove("slds-hide");
-    searchBoxWrapper.classList.add("slds-show");
+    if (searchBoxWrapper) {
+      searchBoxWrapper.classList.remove("slds-hide");
+      searchBoxWrapper.classList.add("slds-show");
+      searchBoxWrapper.querySelector("lightning-input").value =
+        this.selectedRecord;
+    }
     const pillDiv = this.template.querySelector(".pillDiv");
-    pillDiv.classList.remove("slds-show");
-    pillDiv.classList.add("slds-hide");
+    if (pillDiv) {
+      pillDiv.classList.remove("slds-show");
+      pillDiv.classList.add("slds-hide");
+    }
   }
   // method to update selected record from search result
-  handelSelectedRecord(event) {
-    var objId = event.target.getAttribute("data-recid"); // get selected record Id
-    this.selectedRecord = this.lstResult.find((data) => data === objId); // find selected record from list
-    this.lookupUpdatehandler(this.selectedRecord); // update value on parent component as well from helper function
+  handleSelectedRecord(event) {
+    const selectedName = event.target.getAttribute("data-objname"); // get selected record Idrd from list
+    this.selectObject(selectedName);
+    this.lookupUpdatehandler(selectedName); // update value on parent component as well from helper function
+  }
+
+  selectObject(val) {
+    if (!val || val.trim().length === 0) {
+      this.handleRemove();
+      return;
+    }
+    // data isn't ready yet, will auto-retry on data loaded
+    if (!this.objects) {
+      return;
+    }
+    const obj = this.objects.find((o) => o.fullName === val);
+    this.selectedRecord = `${obj.label} (${obj.fullName})`;
+    this.selectedRecordIconName = obj.iconName;
     this.handelSelectRecordHelper(); // helper function to show/hide lookup result container on UI
   }
+
   /*COMMON HELPER METHOD STARTED*/
   handelSelectRecordHelper() {
     this.template
@@ -103,10 +227,8 @@ export default class ObjectSelector extends LightningElement {
   // send selected lookup record to parent component using custom event
   lookupUpdatehandler(value) {
     //Send only the API Name in the event
-    var regExp = /\(([^)]+)\)/;
-    var retVal = regExp.exec(JSON.stringify(value));
     const oEvent = new CustomEvent("lookupupdate", {
-      detail: { selectedRecord: retVal[1] }
+      detail: { selectedRecord: value }
     });
     this.dispatchEvent(oEvent);
   }
